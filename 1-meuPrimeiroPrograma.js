f4f4d14cfe6992d87e4ffe165f31f913ad42b123
sibellecarvalho <164205216+sibellecarvalho@users.noreply.github.com>
2024-09-27 11:15:09 -0300

    Create 1-meuPrimeiroPrograma.js

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("Meu Primeiro programa: trabalhando com variávei");
+
+const idade = 26;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idae-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
